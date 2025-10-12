201b4936fc34f3929736de37a6242ce31230d9ff
Theletsane <theletsanei47@gmail.com>
2025-10-12 18:12:40 +0200

    Starting with the animation style. -Generating animations with chatGPT to see different animations styles Id like to use.

---
+++ GUI/src/Dongle_Gui.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QPropertyAnimation, QRect
+
+class SimpleAnimation(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt5 Animation Demo")
+        self.resize(400, 200)
+
+        # Button to animate
+        self.button = QPushButton("Click Me", self)
+        self.button.setGeometry(20, 80, 100, 40)
+        self.button.clicked.connect(self.start_animation)
+
+    def start_animation(self):
+        # Create an animation object targeting the button's geometry
+        self.anim = QPropertyAnimation(self.button, b"geometry")
+        self.anim.setDuration(2000)  # duration in ms
+        self.anim.setStartValue(QRect(20, 80, 100, 40))
+        self.anim.setEndValue(QRect(300, 150, 100, 40))
+        self.anim.start()
+
+app = QApplication(sys.argv)
+window = SimpleAnimation()
+window.show()
+sys.exit(app.exec_())
